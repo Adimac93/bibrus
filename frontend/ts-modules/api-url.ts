a17007e1c02d75ef2888d4e340b4b003f31afe29
Mabi19 <mateuszbartnik33@gmail.com>
2022-10-07 17:51:35 +0200

    fix(frontend): remove debug console.log

--- frontend/ts-modules/api-url.ts
+++ frontend/ts-modules/api-url.ts
@@ -3,6 +3,5 @@ import { useRuntimeConfig } from "#imports";
 export function apiURL(url: string) {
     if (url.startsWith("/"))
         url = url.slice(1);
-    console.log(useRuntimeConfig().public.apiURL);
     return useRuntimeConfig().public.apiURL + url;
 }
